66c9065ec317575059d0a26bfb5a170f2e21b392
Huigesi <791339970@qq.com>
2018-10-09 17:17:17 +0800

    section4

--- app/src/main/java/me/huigesi/androidqunyinzhuang/MainActivity.java
+++ app/src/main/java/me/huigesi/androidqunyinzhuang/MainActivity.java
@@ -2,12 +2,25 @@ package me.huigesi.androidqunyinzhuang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TopBar mTopBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTopBar.setOnTopbarClickListener(new topbarClickListener() {
+            @Override
+            public void leftClick() {
+
+            }
+
+            @Override
+            public void rightClick() {
+
+            }
+        });
     }
 }
